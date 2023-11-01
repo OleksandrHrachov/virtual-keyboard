10a57a8548c2fa2b6e83618d8a943e13f5b7c99f
o.hrachov <ol.grachov@gmail.com>
2023-11-01 17:07:04 +0200

    added highlight when pressing on the keyboard

--- script.js
+++ script.js
@@ -1,69 +1,177 @@
 const BODY = document.querySelector("body");
-
-const keyboardRows = [
-  [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 'Backspace'],
-  ['Tab', 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 'DEL'],
-  ['CapsLock', 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 69],
-  ['Shift', 92, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 'up', 'Shift'],
-  ['Ctrl', 'Win', 'Alt', 32, 'Alt', 'Ctrl', 'left', 'down', 'right'],
-];
-
-const biggerBtns = [
-  "Backspace",
-  "Tab",
-  "DEL",
-  "CapsLock",
-  69,
-  "Shift",
-  "Ctrl",
-  'Space',
-];
-
 const virtualArea = document.createElement("div");
 virtualArea.classList.add("virtual-area");
 BODY.appendChild(virtualArea);
 
 const textArea = document.createElement("textarea");
 textArea.classList.add("textarea");
-textArea.setAttribute("rows", "10");
+textArea.setAttribute("rows", "7");
+textArea.setAttribute("tabIndex", "0");
 virtualArea.appendChild(textArea);
+
+const keyboardRowsValue = [
+  [
+    { mainValue: "`", secondaryValue: "~" },
+    { mainValue: "1", secondaryValue: "!" },
+    { mainValue: "2", secondaryValue: "@" },
+    { mainValue: "3", secondaryValue: "#" },
+    { mainValue: "4", secondaryValue: "$" },
+    { mainValue: "5", secondaryValue: "%" },
+    { mainValue: "6", secondaryValue: "^" },
+    { mainValue: "7", secondaryValue: "&" },
+    { mainValue: "8", secondaryValue: "*" },
+    { mainValue: "9", secondaryValue: "(" },
+    { mainValue: "0", secondaryValue: ")" },
+    { mainValue: "-", secondaryValue: "_" },
+    { mainValue: "=", secondaryValue: "+" },
+    { mainValue: "backspace", secondaryValue: null },
+  ],
+  [
+    { mainValue: "tab", secondaryValue: null },
+    { mainValue: "q", secondaryValue: null },
+    { mainValue: "w", secondaryValue: null },
+    { mainValue: "e", secondaryValue: null },
+    { mainValue: "r", secondaryValue: null },
+    { mainValue: "t", secondaryValue: null },
+    { mainValue: "y", secondaryValue: null },
+    { mainValue: "u", secondaryValue: null },
+    { mainValue: "i", secondaryValue: null },
+    { mainValue: "o", secondaryValue: null },
+    { mainValue: "p", secondaryValue: null },
+    { mainValue: "[", secondaryValue: "{" },
+    { mainValue: "]", secondaryValue: "}" },
+    { mainValue: "\\", secondaryValue: "|" },
+    { mainValue: "del", secondaryValue: null },
+  ],
+  [
+    { mainValue: "caps lock", secondaryValue: null },
+    { mainValue: "a", secondaryValue: null },
+    { mainValue: "s", secondaryValue: null },
+    { mainValue: "d", secondaryValue: null },
+    { mainValue: "f", secondaryValue: null },
+    { mainValue: "g", secondaryValue: null },
+    { mainValue: "h", secondaryValue: null },
+    { mainValue: "j", secondaryValue: null },
+    { mainValue: "k", secondaryValue: null },
+    { mainValue: "l", secondaryValue: null },
+    { mainValue: ";", secondaryValue: ":" },
+    { mainValue: "'", secondaryValue: "double_quote" },
+    { mainValue: "enter", secondaryValue: null },
+  ],
+  [
+    { mainValue: "shift", secondaryValue: null },
+    { mainValue: "\\", secondaryValue: null },
+    { mainValue: "z", secondaryValue: null },
+    { mainValue: "x", secondaryValue: null },
+    { mainValue: "c", secondaryValue: null },
+    { mainValue: "v", secondaryValue: null },
+    { mainValue: "b", secondaryValue: null },
+    { mainValue: "n", secondaryValue: null },
+    { mainValue: "m", secondaryValue: null },
+    { mainValue: ",", secondaryValue: null },
+    { mainValue: ".", secondaryValue: null },
+    { mainValue: "/", secondaryValue: null },
+    { mainValue: "up", secondaryValue: null },
+    { mainValue: "shift", secondaryValue: null },
+  ],
+  [
+    { mainValue: "ctrl", secondaryValue: null },
+    { mainValue: "win", secondaryValue: null },
+    { mainValue: "alt", secondaryValue: null },
+    { mainValue: "space", secondaryValue: null },
+    { mainValue: "alt", secondaryValue: null },
+    { mainValue: "ctrl", secondaryValue: null },
+    { mainValue: "left", secondaryValue: null },
+    { mainValue: "down", secondaryValue: null },
+    { mainValue: "right", secondaryValue: null },
+  ],
+];
+
+const addToTextarea = (symbol) => {
+  textArea.value += symbol;
+};
+
+const biggerBtns = [
+  "backspace",
+  "tab",
+  "del",
+  "caps lock",
+  "enter",
+  "shift",
+  "ctrl",
+  "space",
+];
+
+const serviceBtns = [
+  "backspace",
+  "tab",
+  "del",
+  "caps lock",
+  "enter",
+  "shift",
+  "ctrl",
+  "space",
+  "win",
+  "alt",
+  "space",
+  "up",
+  "left",
+  "down",
+  "right",
+];
 
 const createKey = (key, index, arr) => {
   const btn = document.createElement("div");
   btn.classList.add("key");
-  btn.setAttribute('data-key', key);
 
-  if (biggerBtns.includes(key)) {
+  if (key.mainValue === "\\") {
+    btn.setAttribute("data-key", "backslash");
+  } else {
+    btn.setAttribute("data-key", key.mainValue);
+  }
+
+  if (biggerBtns.includes(key.mainValue)) {
     btn.classList.add("key--big");
   }
-  if (key === "Shift" && index === arr.length - 1) {
+  if (key.mainValue === "shift" && index === arr.length - 1) {
     btn.classList.remove("key--big");
   }
-  if (key === 32) {
+  if (key.mainValue === "space") {
     btn.classList.add("key--space");
   }
-  if (typeof key === 'string' || key === 32 || key === 69) {
-    btn.classList.add('special-btn');
+  if (serviceBtns.includes(key.mainValue)) {
+    btn.classList.add("service-btn");
   }
 
-  if (key === "up") {
+  if (key.mainValue === "up") {
     btn.innerText = String.fromCodePoint(11205);
-    btn.classList.add('arrow');
-  } else if (key === "down") {
+    btn.classList.add("arrow");
+  } else if (key.mainValue === "down") {
     btn.innerText = String.fromCodePoint(11206);
-    btn.classList.add('arrow');
-  } else if (key === "left") {
+    btn.classList.add("arrow");
+  } else if (key.mainValue === "left") {
     btn.innerText = String.fromCodePoint(11207);
-    btn.classList.add('arrow');
-  } else if (key === "right") {
+    btn.classList.add("arrow");
+  } else if (key.mainValue === "right") {
     btn.innerText = String.fromCodePoint(11208);
-    btn.classList.add('arrow');
-  }else if (key === 69) {
-    btn.innerText = 'Enter';
-  } else if (typeof key === 'string') {
-    btn.innerText = key;
+    btn.classList.add("arrow");
   } else {
-    btn.innerText = String.fromCharCode(key);
+    btn.innerText = key.mainValue;
+  }
+
+  if (key.secondaryValue) {
+    const btnSecondary = document.createElement("div");
+    btnSecondary.classList.add("key--secondary");
+
+    if (key.secondaryValue === "double_quote") {
+      btnSecondary.setAttribute("data-key", key.secondaryValue);
+      btnSecondary.innerText = '"';
+    } else {
+      btnSecondary.setAttribute("data-key", key.secondaryValue);
+      btnSecondary.innerText = key.secondaryValue;
+    }
+
+    btn.appendChild(btnSecondary);
   }
 
   return btn;
@@ -98,24 +206,51 @@ const createKeyboard = (keyboardRows) => {
   return keyboard;
 };
 
-const keyboardKeysRows = createRows(keyboardRows);
+const keyboardKeysRows = createRows(keyboardRowsValue);
 
 const keyboard = createKeyboard(keyboardKeysRows);
 virtualArea.appendChild(keyboard);
 
-document.addEventListener('keypress', (e) => {
-  console.log('charCode =>', e.key.charCodeAt());
-  console.log('letter =>', String.fromCharCode(e.key.charCodeAt()));
+const getDataKey = (key) => {
+  switch (key) {
+    case "\\":
+      return "backslash";
+    case " ":
+      return "space";
+    case "Delete":
+      return "del";
+    case "CapsLock":
+      return "caps lock";
+    case "Control":
+      return "ctrl";
+    case "Meta":
+      return "win";
+    case "ArrowUp":
+      return "up";
+    case "ArrowDown":
+      return "down";
+    case "ArrowLeft":
+      return "left";
+    case "ArrowRight":
+      return "right";
+    case '"':
+      return "double_quote";
+    default:
+      return key.toLowerCase();
+  }
+};
 
-  const keyCharCode = e.key.charCodeAt();
-  const keys = document.querySelectorAll(`[data-key='${keyCharCode}']`);
-  console.log('keys =>', keys);
+document.addEventListener("keydown", (e) => {
+  let dataKey = getDataKey(e.key);
+  const keys = document.querySelectorAll(`[data-key="${dataKey}"]`);
+
+  textArea.focus();
 
   keys.forEach((btn) => {
-    btn.classList.add('active');
+    btn.classList.add("active");
 
     setTimeout(() => {
-      btn.classList.remove('active');
+      btn.classList.remove("active");
     }, 200);
-  })
-})
+  });
+});
